1f6bd3884273897372276e2ab38565a2b19aa494
mikedparrott <mikedparrott@gmail.com>
2021-03-03 14:25:25 -0500

    change success reponse for ai mod share creds
    @TheStreett

--- aimodelshare/aws.py
+++ aimodelshare/aws.py
@@ -39,7 +39,7 @@ def set_credentials(credential_file=None, type="submit_model", apiurl="apiurl", 
   #Validate Username & Password
   try: 
     token=ai.aws.get_aws_token(os.environ.get("username"), os.environ.get("password"))
-    print("AI Modelshare Login Credentials set successfully.")
+    print("AI Model Share login credentials set successfully.")
   except: 
     print("Credential confirmation unsuccessful. Check username & password and try again.")
     return
