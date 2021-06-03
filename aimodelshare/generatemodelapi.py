752eac95f0b0bc5abb801daba7311b81a3e71661
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2021-06-03 12:44:31 -0400

    delete extra =

--- aimodelshare/generatemodelapi.py
+++ aimodelshare/generatemodelapi.py
@@ -236,7 +236,7 @@ def send_model_data_to_dyndb_and_return_api(api_info, private, categorical, prep
     return print(finalresult2)
 
 
-def model_to_api(model_filepath, model_type, private, categorical, trainingdata, y_train,preprocessor_filepath,custom_libraries=="FALSE"):
+def model_to_api(model_filepath, model_type, private, categorical, trainingdata, y_train,preprocessor_filepath,custom_libraries="FALSE"):
     """
       Launches a live prediction REST API for deploying ML models using model parameters and user credentials, provided by the user
       Inputs : 8
