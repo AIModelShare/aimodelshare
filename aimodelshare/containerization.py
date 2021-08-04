17b2a637798918001b3b68335e3f0fb2f9e0acb0
Vedang Mandhana <mvedang@outlook.com>
2021-08-04 11:56:20 -0400

    Path to model.py wrong in lambda function template

--- aimodelshare/containerization.py
+++ aimodelshare/containerization.py
@@ -363,8 +363,8 @@ def create_lambda_using_base_image(user_session, bucket_name, directory, lambda_
     # attaching policies to role to execute CodeBuild to build Docker image
     attach_policy_to_role(user_session, role_name, policy_name)
 
-    print("Creating Lambda function " + lambda_name + "\".")
-    lambda_client = user_session.client('lambda')
+    print("Creating Lambda function \"" + lambda_name + "\".")
+    lambda_client = user_session.client("lambda")
     counter=1
     while(counter<=3):
         try:
