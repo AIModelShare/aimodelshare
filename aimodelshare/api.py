8317810418386a444e5d06a41f2f56038b451ca1
Vedang Mandhana <mvedang@outlook.com>
2021-08-24 15:52:50 -0400

    Add requirements_file_path

--- aimodelshare/api.py
+++ aimodelshare/api.py
@@ -404,10 +404,12 @@ def create_prediction_api(model_filepath, unique_model_id, model_type, categoric
         with open(os.path.join(file_objects_folder_path, 'requirements.txt'), 'a') as f:
             for lib in requirements:
                 f.write('%s\n' % lib)
-                
+        
+        requirements_file_path = os.path.join(file_objects_folder_path, 'requirements.txt')
+
         from aimodelshare import deploy_container
         #response6 = deploy_lambda_using_sam(user_session, os.getenv("BUCKET_NAME"), requirements, file_objects_folder_path, lambdafxnname, apiid, 1024, 90, "3.7")
-        response6 = deploy_container(account_number, os.environ.get("AWS_REGION"), user_session, lambdafxnname, file_objects_folder_path,apiid)
+        response6 = deploy_container(account_number, os.environ.get("AWS_REGION"), user_session, lambdafxnname, file_objects_folder_path,requirements_file_path,apiid)
 
     response6evalfxn = lambdaclient.create_function(FunctionName=lambdaevalfxnname, Runtime='python3.7', Role='arn:aws:iam::'+account_number+':role/'+lambdarolename, Handler='main.handler',
                                           Code={
