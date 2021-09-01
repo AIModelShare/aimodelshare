e87e92a9de3c8f583aa1a223e976a8a80241a13d
TheStreett <60371043+TheStreett@users.noreply.github.com>
2021-09-01 16:32:10 -0400

    update Quickstart func

--- aimodelshare/data_sharing/download_data.py
+++ aimodelshare/data_sharing/download_data.py
@@ -187,19 +187,10 @@ def download_data(repository):
 def import_quickstart_data():
     from aimodelshare.data_sharing.download_data import download_data
     import tensorflow as tf
-    import pandas as pd
     import os
     import pickle
     
-     # Confirm that creds are loaded, print warning if not
-    if all(["AWS_ACCESS_KEY_ID" in os.environ, 
-            "AWS_SECRET_ACCESS_KEY" in os.environ,
-            "AWS_REGION" in os.environ,
-           "username" in os.environ, 
-           "password" in os.environ]):
-        pass
-    else:
-        return print("'Download unsuccessful. Please provide credentials with set_credentials().")
+     
     
     #Download Quick Start materials
     quickstart_repository = "public.ecr.aws/y2e2a1d6/quickstart_materials-repository:latest"
@@ -222,10 +213,9 @@ def import_quickstart_data():
 
 
 
-def import_quickstart_data_part2():
+def import_quickstart_data(tutorial, section="modelplayground"):
     from aimodelshare.data_sharing.download_data import download_data
     import tensorflow as tf
-    import pandas as pd
     import os
     import pickle
     import shutil 
@@ -241,36 +231,51 @@ def import_quickstart_data_part2():
         return print("'Download unsuccessful. Please provide credentials with set_credentials().")
     
     #Download Quick Start materials
-    quickstart_repository = "public.ecr.aws/y2e2a1d6/quickstart_materials_part2-repository:latest"
+    if all([tutorial == "flowers", section == "modelplayground"]):
+        quickstart_repository = "public.ecr.aws/y2e2a1d6/quickstart_materials-repository:latest"     
+    if all([tutorial == "flowers", section == "competition"]):
+        quickstart_repository = "public.ecr.aws/y2e2a1d6/quickstart_flowers_competition-repository:latest"
     download_data(quickstart_repository)
+
+    #{{{ prepare modelplayground materials
+    if section == "modelplayground": 
+        #Instantiate Model 
+        print("\nPreparing downloaded files for use...")
+        model = tf.keras.models.load_model('quickstart_materials/flowermodel.h5')
+            
+        #unpack data
+        with open("quickstart_materials/y_test_labels.txt", "rb") as fp:  
+            y_test_labels = pickle.load(fp)
+    #}}}
+
+    #{{{ prepare competition materials
+    if section == "competition":
+        #Instantiate Model 
+        print("\nPreparing downloaded files for use...")
+        model_2 = tf.keras.models.load_model('quickstart_flowers_competition/flowermodel_2.h5')
     
-    #Instantiate Model 
-    print("\nPreparing downloaded files for use...")
-    model_2 = tf.keras.models.load_model('quickstart_materials_part2/flowermodel_2.h5')
-    
-    #unpack data
-    with open("quickstart_materials_part2/X_test.pkl", "rb") as fp:  
-        X_test = pickle.load(fp)
-    
-    with open("quickstart_materials_part2/X_train.pkl", "rb") as fp:  
-        X_train = pickle.load(fp)
-    
-    y_train = pd.read_csv("quickstart_materials_part2/y_train.csv")
-    y_test = pd.read_csv("quickstart_materials_part2/y_test.csv")
-    
-    #move data files to folder to upload with create_competiton
-    os.mkdir('data-directory')
-    
-    files = ['quickstart_materials_part2/X_train.pkl', 
-             'quickstart_materials_part2/X_test.pkl', 
-             'quickstart_materials_part2/y_train.csv']
-    
-    for f in files:
-        shutil.move(f, 'data-directory')
+        #unpack data
+        with open("quickstart_flowers_competition/y_test_labels.txt", "rb") as fp:  
+            y_test_labels = pickle.load(fp)
+            
+        #move data files to folder to upload with create_competiton
+        os.mkdir('data-directory')
+            
+        folders = ['quickstart_flowers_competition/test_images', 
+                   'quickstart_flowers_competition/train_images']
+            
+        for f in folders:
+            shutil.move(f, 'data-directory')
+    #}}}
 
     success_message = ("\nSuccess! Your Quick Start materials have been downloaded. \n"
                        "You are now ready to run the tutorial.")
     
     print(success_message)
-    return model_2, X_test, X_train, y_train, y_test
+
+    if section == "modelplayground":
+        return model, y_test_labels
+
+    if section == "competition": 
+        return model_2, y_test_labels
 
