bd8ff210d67087619bf8be492d5ebe9624244921
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2021-11-03 12:09:24 -0400

    add a bit more to init file for current tutorials

--- aimodelshare/__init__.py
+++ aimodelshare/__init__.py
@@ -1,5 +1,6 @@
 from .object_oriented import ModelPlayground, Competition, Data
-
+from .preprocessormodules import export_preprocessor,upload_preprocessor,import_preprocessor
+from .data_sharing.download_data import download_data, import_quickstart_data
 
 #import .sam as sam
 
@@ -7,5 +8,12 @@ __all__ = [
     # Object Oriented
     ModelPlayground,
     Competition,
-    Data
+    Data,
+    # Preprocessor
+    upload_preprocessor,
+    import_preprocessor,
+    export_preprocessor,
+    download_data,
+    share_data_codebuild,
+    share_dataset,
 ]
