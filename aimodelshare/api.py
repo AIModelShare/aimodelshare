2ab0adf99109ae5bbad884577108af5d36a16207
Vedang Mandhana <mvedang@outlook.com>
2022-03-09 21:49:50 -0500

    Add eu west 1, 2, 3 support

--- aimodelshare/api.py
+++ aimodelshare/api.py
@@ -90,14 +90,20 @@ class create_prediction_api_class():
             "us-east-1": "arn:aws:lambda:us-east-1:517169013426:layer:eval_layer_test:6",
             "us-east-2": "arn:aws:lambda:us-east-2:517169013426:layer:eval_layer_test:5",
             "us-west-1": "arn:aws:lambda:us-west-1:517169013426:layer:eval_layer_test:1",
-            "us-west-2": "arn:aws:lambda:us-west-2:517169013426:layer:eval_layer_test:1"
+            "us-west-2": "arn:aws:lambda:us-west-2:517169013426:layer:eval_layer_test:1",
+            "eu-west-1": "arn:aws:lambda:eu-west-1:517169013426:layer:eval_layer_test:1",
+            "eu-west-2": "arn:aws:lambda:eu-west-2:517169013426:layer:eval_layer_test:1",
+            "eu-west-3": "arn:aws:lambda:eu-west-3:517169013426:layer:eval_layer_test:1"
         }
 
         self.auth_layer_map = {
             "us-east-1": "arn:aws:lambda:us-east-1:517169013426:layer:aimsauth_layer:2",
             "us-east-2": "arn:aws:lambda:us-east-2:517169013426:layer:aimsauth_layer:9",
             "us-west-1": "arn:aws:lambda:us-west-1:517169013426:layer:aimsauth_layer:1",
-            "us-west-2": "arn:aws:lambda:us-west-2:517169013426:layer:aimsauth_layer:1"
+            "us-west-2": "arn:aws:lambda:us-west-2:517169013426:layer:aimsauth_layer:1",
+            "eu-west-1": "arn:aws:lambda:eu-west-1:517169013426:layer:aimsauth_layer:1",
+            "eu-west-2": "arn:aws:lambda:eu-west-2:517169013426:layer:aimsauth_layer:1",
+            "eu-west-3": "arn:aws:lambda:eu-west-3:517169013426:layer:aimsauth_layer:1"
         }
 
         onnx_size = math.ceil(os.path.getsize(model_filepath)/(1024*1024))
