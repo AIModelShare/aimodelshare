49c958abd286c8b44d4f4a7e5f9c747db9a3adb0
Vijay S Kalmath <vijay.kalmath363@gmail.com>
2022-09-18 16:30:19 -0400

    Adding AssertionError for instantiate_model in ModelPlayground

--- aimodelshare/object_oriented.py
+++ aimodelshare/object_oriented.py
@@ -219,6 +219,9 @@ class ModelPlayground:
         
     def instantiate_model(self, version=None, trained=False, reproduce=False, submission_type="competition"): 
         """
+
+        DOES NOT WORK FOR MODELPLAYGROUND , ADD corresponding description.
+
         Import a model previously submitted to a leaderboard to use in your session
 
         Parameters:
@@ -232,6 +235,7 @@ class ModelPlayground:
         --------
         model: model chosen from leaderboard
         """
+        raise AssertionError("You are trying to Instantiate model with ModelPlayground Object, Please use the competition object to Instantiate model")
         from aimodelshare.aimsonnx import instantiate_model
         model = instantiate_model(apiurl=self.playground_url, trained=trained, version=version, reproduce=reproduce, submission_type=submission_type)
         return model
