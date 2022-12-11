1aa4d508809337cfcd0b25ae1c7f221f7efbeb03
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2022-12-11 13:13:17 -0500

    update api_key in eval lambda

--- aimodelshare/api.py
+++ aimodelshare/api.py
@@ -209,14 +209,15 @@ class create_prediction_api_class():
             file.write(data)
         
         ###
-        t = Template(pkg_resources.read_text(main, 'eval_lambda.txt'))
+        api_key = str(shortuuid.uuid())
+
+        t = Template(pkg_resources.read_text(main, 'eval_lambda.txt').replace("$apikey",api_key))
         data = t.substitute(bucket_name = self.bucket_name, unique_model_id = self.unique_model_id, task_type = self.task_type)
         with open(os.path.join(self.temp_dir, 'main.py'), 'w') as file:
             file.write(data)
         with ZipFile(os.path.join(self.temp_dir, 'archive2.zip'), 'a') as z:
             z.write(os.path.join(self.temp_dir, 'main.py'), 'main.py')
         self.aws_client.upload_file_to_s3(os.path.join(self.temp_dir, 'archive2.zip'), os.environ.get("BUCKET_NAME"), self.unique_model_id+"/"+'archiveeval.zip')
-        api_key = str(shortuuid.uuid())
 
         data2 = pkg_resources.read_text(main, 'authorization.txt').replace("$apikey",api_key)
         with open(os.path.join(self.temp_dir, 'main.py'), 'w') as file:
