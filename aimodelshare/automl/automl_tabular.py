1dfefeab5384bc5af6b7b8740e88f4abb9839afd
Hu Mingzhe <hmz_superb@qq.com>
2022-12-16 00:43:24 -0500

    a placeholder for integrating aimodelshare

--- aimodelshare/automl/automl_tabular.py
+++ aimodelshare/automl/automl_tabular.py
@@ -1,11 +1,9 @@
-import aimodelshare as ai
-import dabl
-from supervised.automl import AutoML
-
 import os
 import shutil
+import glob
 import numpy as np
 import pandas as pd
+from typing import Any, Sequence
 
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.ensemble import StackingClassifier
@@ -19,19 +17,34 @@ import xgboost
 import lightgbm
 from lightgbm import LGBMClassifier
 from xgboost import XGBClassifier
-from onnxmltools import convert_sklearn
 from onnxmltools.convert.xgboost.operator_converters.XGBoost import (
     convert_xgboost)
 from onnxmltools.convert import convert_xgboost as convert_xgboost_booster
 from onnxmltools.convert.lightgbm.operator_converters.LightGbm import convert_lightgbm
 
-import argparse
-
-from typing import Any, Sequence
+import dabl
+from supervised.automl import AutoML
+import aimodelshare as ai
+from aimodelshare import ModelPlayground
+from aimodelshare.aws import configure_credentials, set_credentials
+from aimodelshare.aimsonnx import model_to_onnx
 
 
 class autoMLTabular:
-    def __init__(self, dataset_names: Sequence, keyword="label", automl_dir="automl", mode=None, **kwargs):
+    def __init__(self, dataset_names: Sequence,
+                 keyword="label",
+                 automl_dir="automl",
+                 mode=None,
+                 playground: ModelPlayground = None,
+                 **kwargs):
+        """
+        :param dataset_names: There are a few possibilities, a series of csv file, a directory or a keyword
+        :param keyword: represent a label keyword
+        :param automl_dir:
+        :param mode:
+        :param playground:
+        :param kwargs:
+        """
         self.datasets_train = None
         self.labels_train = None
         self.datasets_test = None
@@ -39,11 +52,19 @@ class autoMLTabular:
         self.keyword = keyword  # This is the name for the ground truth column
 
         self.automl_dir = automl_dir
-        self.automl = AutoML(results_path=automl_dir, mode=mode if mode else "Perform")
+        self.automl = AutoML(results_path=automl_dir, mode=mode if mode else "Explain")
 
-        self.dataset_names = dataset_names
-        if isinstance(self.dataset_names, list):
-            self.datasets_train, self.datasets_test = pd.read_csv(self.dataset_names[0]), pd.read_csv(self.dataset_names[1])
+        if isinstance(dataset_names, list) or isinstance(dataset_names, tuple):
+            self.datasets_train, self.datasets_test = pd.read_csv(dataset_names[0]), pd.read_csv(
+                dataset_names[1])
+            self.labels_train = self.datasets_train.loc[keyword]
+            self.datasets_train = self.datasets_train.drop([keyword], axis=1)
+            self.labels_test = self.datasets_test.loc[keyword]
+            self.datasets_test = self.datasets_test.drop([keyword], axis=1)
+        elif isinstance(dataset_names, str) and os.path.isdir(dataset_names):
+            dataset_names = sorted(glob.glob("/".join((dataset_names, "*.csv"))), reverse=True)  # train.csv, test.csv
+            self.datasets_train, self.datasets_test = pd.read_csv(dataset_names[0]), pd.read_csv(
+                dataset_names[1])
             self.labels_train = self.datasets_train.loc[keyword]
             self.datasets_train = self.datasets_train.drop([keyword], axis=1)
             self.labels_test = self.datasets_test.loc[keyword]
@@ -51,13 +72,32 @@ class autoMLTabular:
         else:
             # Now it is not working
             try:
-                self.datasets_train, self.datasets_test, self.labels_train, self.labels_test, example_data, y_test_labels = ai.import_quickstart_data(dataset_names)
+                self.datasets_train, self.datasets_test, self.labels_train, self.labels_test, example_data, y_test_labels = ai.import_quickstart_data(
+                    dataset_names)
             except:
-                self.datasets_train, self.datasets_test, self.labels_train, self.labels_test = kwargs["datasets_train"], kwargs["datasets_test"], kwargs["labels_train"], kwargs["labels_test"]
+                self.datasets_train, self.datasets_test, self.labels_train, self.labels_test, example_data = kwargs["datasets_train"], \
+                                                                                               kwargs["datasets_test"], \
+                                                                                               kwargs["labels_train"], \
+                                                                                               kwargs["labels_test"], \
+                                                                                               kwargs.get("example_data", None)
         self.preprocessor = dabl.EasyPreprocessor()  # This is for fast preprocessing
 
         self._fast_preprocessing_fit()
         self._fast_preprocessing_transform()
+
+        self.configure_credential()
+
+        if playground is None or playground.playground_url is None:
+            # Create a new competition
+            playground = ModelPlayground(model_type="tabular", classification=True, private=False)
+            try:
+                playground.deploy("model.onnx", "preprocessor.zip", self.labels_train, example_data)
+            except:
+                playground.deploy("model.onnx", "preprocessor.zip", self.labels_train, pd.DataFrame(self.datasets_train[0:4]))
+            playground.create_competition(data_directory='competition_data',
+                                                      y_test=self.labels_test)
+        self.competition_playground = ai.Competition(playground.playground_url)
+        self.set_credential(apiurl=playground.playground_url)
 
     def _fast_preprocessing_fit(self):
         if self.datasets_train is not None:
@@ -80,7 +120,7 @@ class autoMLTabular:
         if self.labels_train.dtype == int:
             self.labels_train_processed = self.labels_train.copy()
             return
-        self.labels_train_processed = np.empty((len(self.labels_train), ))
+        self.labels_train_processed = np.empty((len(self.labels_train),))
         if self.labels_train is not None:
             if isinstance(self.labels_train, pd.DataFrame):
                 unique_labels = pd.unique(self.labels_train)
@@ -96,7 +136,7 @@ class autoMLTabular:
         if self.labels_test.dtype == int:
             self.labels_test_processed = self.labels_test.copy()
             return
-        self.labels_test_processed = np.empty((len(self.labels_test), ))
+        self.labels_test_processed = np.empty((len(self.labels_test),))
         if self.labels_test is not None:
             i = 0
             for label in self.labels_test:
@@ -125,11 +165,15 @@ class autoMLTabular:
         assert self.datasets_test is not None
         return self.automl.score(self.datasets_test, self.labels_test_processed)
 
-    def get_leaderboard(self):
+    def get_leaderboard(self, competition=False):
         """
         Get leaderboard of different models
         """
-        print(self.automl.get_leaderboard())
+        if competition:
+            data = self.competition_playground.get_leaderboard()
+            self.competition_playground.stylize_leaderboard(data)
+        else:
+            print(self.automl.get_leaderboard())
 
     def get_ensemble_model(self):
         """
@@ -165,8 +209,9 @@ class autoMLTabular:
                         calculate_linear_classifier_output_shapes, convert_lightgbm,
                         options={'nocl': [True, False], 'zipmap': [True, False, 'columns']})
                 else:
-                    raise NotImplementedError
-                assert hasattr(model, "predict")  # and hasattr(preprocessor, "fit") and hasattr(preprocessor, "transform")
+                    continue
+                assert hasattr(model,
+                               "predict")  # and hasattr(preprocessor, "fit") and hasattr(preprocessor, "transform")
                 pipeline = new_model
             else:
                 pipeline = model
@@ -178,16 +223,20 @@ class autoMLTabular:
                                                 final_estimator=LogisticRegression())
 
         pipeline = make_pipeline(stacked_classifier)
-        self._fit_pipeline(pipeline)
-        return pipeline, preprocessor
+        packed_fitting = self._fit_pipeline(pipeline)
+        predicted_labels = packed_fitting[1]
+        return pipeline, preprocessor, predicted_labels
 
     def _fit_pipeline(self, pipeline):
         """
         Fit pipeline for onnx save
         """
         pipeline.fit(self.datasets_train, self.labels_train_processed)
-        score_ = pipeline.score(self.datasets_test, self.labels_test_processed)
-        return pipeline, score_
+        predicted_labels = pipeline.predict(self.datasets_test)
+        if self.labels_test_processed is not None:
+            score_ = pipeline.score(self.datasets_test, self.labels_test_processed)
+            return pipeline, predicted_labels, score_
+        return pipeline, predicted_labels
 
     def reset(self):
         if os.path.exists(self.automl_dir):
@@ -195,29 +244,67 @@ class autoMLTabular:
 
     def to_onnx(self, pipeline_fitted):
         initial_types = [('float_input', FloatTensorType([None, self.datasets_train.shape[1]]))]
-        onnx_model = convert_sklearn(
-            pipeline_fitted, 'pipeline_ensemble',
-            initial_types,
-            target_opset={'': 12, 'ai.onnx.ml': 2})
+        onnx_model = model_to_onnx(pipeline_fitted,
+                                   initial_types=initial_types,
+                                   framework='sklearn',
+                                   transfer_learning=False,
+                                   deep_learning=False)
+        with open("model.onnx", "wb") as f:
+            f.write(onnx_model.SerializeToString())
         return onnx_model
 
+    @staticmethod
+    def configure_credential():
+        if not os.path.exists('credentials.txt'):
+            configure_credentials()
+        else:
+            print("Credentials already exist!")
 
-def automl_tabular(datasets_name, automl_dir="automl", keyword="label", mode=None, **kwargs):
-    modulize = autoMLTabular(datasets_name, automl_dir, keyword, mode=mode, **kwargs)
-    modulize.reset()
-    modulize.train()
-    print("Prediction score", modulize.prediction_score())
-    modulize.get_leaderboard()
-    pipeline, preprocessor = modulize.get_ensemble_model()
-    # onnx_model = modulize.to_onnx(pipeline)
-    return pipeline, preprocessor
+    @staticmethod
+    def set_credential(apiurl=None):
+        set_credentials(credential_file="credentials.txt", type="deploy_model")
+        if apiurl:
+            set_credentials(apiurl=apiurl)
+
+    @staticmethod
+    def zip_preprocessor(preprocessor):
+        ai.export_preprocessor(preprocessor, "")
+
+    def submit_leaderboard(self, prediction_labels):
+        reversed_mapping = {self.mapping[key]: key for key in self.mapping}
+        if self.competition_playground is not None:
+            self.competition_playground.submit_model(model_filepath="model.onnx",
+                                                     preprocessor_filepath="preprocessor.zip",
+                                                     prediction_submission=list(
+                                                         map(lambda x: reversed_mapping[x], prediction_labels)))
+        else:
+            print("No competition created.")
+
+    def activate(self):
+        self.reset()
+        self.train()
+        print("Prediction score", self.prediction_score())
+        self.get_leaderboard()
+        pipeline, preprocessor, predicted_labels = self.get_ensemble_model()
+        onnx_model = self.to_onnx(pipeline)
+        self.zip_preprocessor(preprocessor)
+        self.submit_leaderboard(predicted_labels)
+        self.get_leaderboard(True)
+        return pipeline, preprocessor
 
 
 if __name__ == "__main__":
     datasets_name = "titanic"
-    datasets_train, datasets_test, labels_train, labels_test, _, _ = ai.import_quickstart_data(datasets_name)
-    pipeline, preprocessor = automl_tabular(datasets_name,
-                                            datasets_train=datasets_train,
-                                            datasets_test=datasets_test,
-                                            labels_train=labels_train,
-                                            labels_test=labels_test)
+    datasets_train, datasets_test, labels_train, labels_test, example_data, _ = ai.import_quickstart_data(datasets_name)
+    myplayground = ModelPlayground(model_type="tabular",
+                                   classification=True,
+                                   private=False)
+    myplayground.playground_url = 'https://die2py70qa.execute-api.us-east-2.amazonaws.com/prod/m'
+    automl = autoMLTabular(datasets_name,
+                           competition_playground=myplayground,
+                           datasets_train=datasets_train,
+                           datasets_test=datasets_test,
+                           labels_train=labels_train,
+                           labels_test=labels_test,
+                           example_data=example_data)
+    pipeline, preprocessor = automl.activate()
