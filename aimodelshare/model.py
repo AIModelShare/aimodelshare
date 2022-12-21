e36f8eda4765871f1c97918a49d8bb261f9b5c06
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2022-12-21 16:11:40 -0500

    fix none in returned value

--- aimodelshare/model.py
+++ aimodelshare/model.py
@@ -1104,11 +1104,10 @@ def submit_model(
                   json=bodydatamodels_allstrings, headers=headers_with_authentication)
 
     if str(response.status_code)=="200":
-        code_comp_result="To submit code used to create this model or to view current leaderboard navigate to Model Playground: \n\n https://www.modelshare.org/detail/model:"+response.text.split(":")[1]  
+        return print(code_comp_result="To submit code used to create this model or to view current leaderboard navigate to Model Playground: \n\n https://www.modelshare.org/detail/model:"+response.text.split(":")[1])  
     else:
-        code_comp_result="" #TODO: reponse 403 indicates that user needs to reset credentials.  Need to add a creds check to top of function.
+        return print("Submission unsuccessful: Reset your credentials to generate new token and try again.") #TODO: reponse 403 indicates that user needs to reset credentials.  Need to add a creds check to top of function.
 
-    return print("\nYour model has been submitted as model version "+str(model_version)+ "\n\n"+code_comp_result)
 
 def update_runtime_model(apiurl, model_version=None, submission_type="competition"):
     """
