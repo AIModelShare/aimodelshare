04da54d98a15b876f71bfc06fa51923a8032cb91
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2022-12-22 14:08:36 -0500

    update urls

--- aimodelshare/object_oriented.py
+++ aimodelshare/object_oriented.py
@@ -318,7 +318,7 @@ class ModelPlayground:
 
 
 
-              apiurl="https://n8567q0ssd.execute-api.us-east-2.amazonaws.com/prod/m"
+              apiurl="https://teljih4cll.execute-api.us-east-2.amazonaws.com/prod/m"
 
               apiurl_eval=apiurl[:-1]+"eval"
 
@@ -404,7 +404,7 @@ class ModelPlayground:
             import requests
             import json
 
-            api_url = "https://ugjaeaenxaerdubk7by2nonw3m0zfchw.lambda-url.us-east-2.on.aws/"
+            api_url = "https://uin6dpubw7lyfh6ew2puk7fzvi0eeqla.lambda-url.us-east-2.on.aws/"
 
             data = json.dumps({"code": """from aimodelshare import ModelPlayground;myplayground="""+deploystring, "zipfilename": deployzipfilename,"username":os.environ.get("username"), "password":os.environ.get("password"),"token":os.environ.get("JWT_AUTHORIZATION_TOKEN"),"s3keyid":"diays4ugz5"})
 
@@ -536,7 +536,7 @@ class ModelPlayground:
 
 
 
-                apiurl="https://n8567q0ssd.execute-api.us-east-2.amazonaws.com/prod/m"
+                apiurl="https://teljih4cll.execute-api.us-east-2.amazonaws.com/prod/m"
 
                 apiurl_eval=apiurl[:-1]+"eval"
 
@@ -628,7 +628,7 @@ class ModelPlayground:
             import requests
             import json
 
-            api_url = "https://ugjaeaenxaerdubk7by2nonw3m0zfchw.lambda-url.us-east-2.on.aws/"
+            api_url = "https://uin6dpubw7lyfh6ew2puk7fzvi0eeqla.lambda-url.us-east-2.on.aws/"
 
             data = json.dumps({"code": """from aimodelshare import ModelPlayground;myplayground="""+compstring, "zipfilename": compzipfilename,"username":os.environ.get("username"), "password":os.environ.get("password"),"token":os.environ.get("JWT_AUTHORIZATION_TOKEN"),"s3keyid":"diays4ugz5"})
 
