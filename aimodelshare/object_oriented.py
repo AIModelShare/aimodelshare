b9ca06bbfb54e056ac3b33a9f9e430b2844df6cb
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2022-12-22 15:34:57 -0500

    update playgroundurlid to self

--- aimodelshare/object_oriented.py
+++ aimodelshare/object_oriented.py
@@ -421,7 +421,7 @@ class ModelPlayground:
             
             print("\nConnect to your playground in Python:\n")
             print("myplayground=ModelPlayground(playground_url="+json.loads(result['body'])[-7].replace("Playground Url: ","").strip()+")")
-            self.playground_url = modelplaygroundid[1:-1]
+            self.playground_url=modelplaygroundurlid
         else:    
         
             #aws pathway begins here
