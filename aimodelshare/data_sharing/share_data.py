1531dd83aa693244c2e2f238eeb3b8bc821008a4
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2022-12-23 10:53:49 -0500

    replace /tmp/ repeat

--- aimodelshare/data_sharing/share_data.py
+++ aimodelshare/data_sharing/share_data.py
@@ -179,7 +179,7 @@ def share_data_codebuild(account_id, region, dataset_dir, dataset_tag='latest', 
     repository=dataset_name+'-repository'
 
     template_folder=tempfile.gettempdir() + '/' + dataset_name+'_'+dataset_tag
-
+    template_folder= template_folder.replace("/tmp//tmp/","/tmp/")
     codebuild_role_name=dataset_name+'-codebuild-role'
     codebuild_policies_name=dataset_name+'-codebuild-policies'
 
