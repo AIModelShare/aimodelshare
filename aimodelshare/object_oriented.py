cfc64e9479eb0c145b9a1137fab6a73565429028
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2022-12-29 12:20:46 -0600

    add printed output with threading
    for cloud deployment

--- aimodelshare/object_oriented.py
+++ aimodelshare/object_oriented.py
@@ -475,39 +475,228 @@ class ModelPlayground:
 
                 print(json.loads(result['body'])[-8]+"\n")
                 print("View live playground now at:\n"+json.loads(result['body'])[-1])
+        if "model_share"==os.environ.get("cloud_location"):
+            print("Creating your Model Playground...\nEst. completion: ~1 minute\n")
 
-                print("\nConnect to your playground in Python:\n")
-                print("myplayground=ModelPlayground(playground_url="+json.loads(result['body'])[-7].replace("Playground Url: ","").strip()+")")
+
+            def deployment_output_information():
+                import os
+                import sys
+
+                sys.stdout.write("[===                                  ] Progress: 5% - Accessing cloud, uploading resources...")
+                sys.stdout.flush()
+                time.sleep(15)
+                sys.stdout.write('\r')
+                sys.stdout.write("[========                             ] Progress: 30% - Building serverless functions and updating permissions...")
+                sys.stdout.flush()
+                time.sleep(15)
+                sys.stdout.write('\r')
+                sys.stdout.write("[============                         ] Progress: 40% - Creating custom containers...                        ")
+                sys.stdout.flush()
+                time.sleep(15)
+                sys.stdout.write('\r')
+                sys.stdout.write("[==========================           ] Progress: 75% - Deploying prediction API...                          ")
+                sys.stdout.flush()
+                time.sleep(10)
+                sys.stdout.write('\r')
+                sys.stdout.write("[================================     ] Progress: 90% - Configuring prediction API...                          ")
+                sys.stdout.flush()
+                time.sleep(10)
+
+            from threading import Thread
+            import time
+
+
+            thread_running = True
+            t1 = Thread(target=deployment_output_information)
+            t1.start()
+
+            def run_deployment_code(model_filepath=model_filepath, 
+                                          model_type = self.model_type, 
+                                          private = self.private, 
+                                          categorical = self.categorical,
+                                          y_train = y_train, 
+                                          preprocessor_filepath = preprocessor_filepath, 
+                                          example_data = example_data,
+                                          custom_libraries = custom_libraries,
+                                          image=image,
+                                          reproducibility_env_filepath = reproducibility_env_filepath,
+                                          memory=memory,
+                                          timeout=timeout,
+                                          email_list=self.email_list,
+                                          pyspark_support=pyspark_support,
+                                          input_dict=input_dict, 
+                                          print_output=False):
+                def upload_playground_zipfile(model_filepath=None, preprocessor_filepath=None, y_train=None, example_data=None):
+                  """
+                  minimally requires model_filepath, preprocessor_filepath 
+                  """
+                  zipfilelist=[model_filepath,preprocessor_filepath]
+
+                  import json
+                  import os
+                  import requests
+                  import pandas as pd
+                  if any([isinstance(example_data, pd.DataFrame),example_data==None]):
+                      pass
+                  else:
+                      zipfilelist.append(example_data)
+
+                  #need to save dict pkl file with arg name and filepaths to add to zipfile
+
+
+
+                  apiurl="https://6far4l0b7i.execute-api.us-east-2.amazonaws.com/prod/m"
+
+                  apiurl_eval=apiurl[:-1]+"eval"
+
+                  headers = { 'Content-Type':'application/json', 'authorizationToken': json.dumps({"token":os.environ.get("AWS_TOKEN"),"eval":"TEST"}), } 
+                  post_dict = {"return_zip": "True"}
+                  zipfile = requests.post(apiurl_eval,headers=headers,data=json.dumps(post_dict)) 
+
+                  zipfileputlistofdicts=json.loads(zipfile.text)['put']
+
+                  zipfilename=list(zipfileputlistofdicts.keys())[0]
+
+                  from zipfile import ZipFile
+                  import os
+                  from os.path import basename
+                  import tempfile
+
+                  wkingdir=os.getcwd()
+
+                  tempdir=tempfile.gettempdir() 
+
+                  zipObj = ZipFile(tempdir+"/"+zipfilename, 'w')
+                  # Add multiple files to the zip
+                  for i in zipfilelist:
+                    zipObj.write(i)
+
+                  # add object to pkl file pathway here. (saving y label data)
+                  import pickle
+
+                  if y_train==None:
+                    pass
+                  else:
+                    with open(tempdir+"/"+'ytrain.pkl', 'wb') as f:
+                      pickle.dump(y_train, f)
+
+                    os.chdir(tempdir)
+                    zipObj.write('ytrain.pkl')
+
+                  if isinstance(example_data, pd.DataFrame):
+                    with open(tempdir+"/"+'exampledata.pkl', 'wb') as f:
+                      pickle.dump(example_data, f)
+
+                    os.chdir(tempdir)
+                    zipObj.write('exampledata.pkl')
+                  else:
+                    pass
+
+
+                  # close the Zip File
+                  os.chdir(wkingdir)
+
+                  zipObj.close()
+
+
+
+                  import ast
+
+                  finalzipdict=ast.literal_eval(zipfileputlistofdicts[zipfilename])
+
+                  url=finalzipdict['url']
+                  fields=finalzipdict['fields']
+
+                  #### save files from model deploy to zipfile in tempdir before loading to s3
+
+
+
+                  ### Load zipfile to s3
+                  with open(tempdir+"/"+zipfilename, 'rb') as f:
+                    files = {'file': (tempdir+"/"+zipfilename, f)}
+                    http_response = requests.post(url, data=fields, files=files)
+                  return zipfilename
+                deployzipfilename=upload_playground_zipfile(model_filepath, preprocessor_filepath, y_train, example_data)   
+                #if aws arg = false, do this, otherwise do aws code
+                #create deploy code_string
+                def nonecheck(objinput=""):
+                    if objinput==None:
+                      objinput="None"
+                    else:
+                      objinput="'/tmp/"+objinput+"'"
+                    return objinput
+                
+                deploystring=self.class_string.replace(",aws=False","")+"."+"deploy('/tmp/"+model_filepath+"','/tmp/"+preprocessor_filepath+"',"+'y_train'+","+nonecheck(example_data)+",input_dict="+str(input_dict)+')'
+                import base64
+                import requests
+                import json
+
+                api_url = "https://nds5eyacuohjseaknx4wpteok40lbjew.lambda-url.us-east-2.on.aws/"
+
+                data = json.dumps({"code": """from aimodelshare import ModelPlayground;myplayground="""+deploystring, "zipfilename": deployzipfilename,"username":os.environ.get("username"), "password":os.environ.get("password"),"token":os.environ.get("JWT_AUTHORIZATION_TOKEN"),"s3keyid":"xrjpv1i7xe"})
+
+                headers = {"Content-Type": "application/json"}
+
+                response = requests.request("POST", api_url, headers = headers, data=data)
+                # Print response
+                global successful_deployment_info340893124738241023
+
+                result=json.loads(response.text)
+                successful_deployment_info340893124738241023=result
+
+                modelplaygroundurlid=json.loads(result['body'])[-7].replace("Playground Url: ","").strip()
                 try:
                     self.playground_url=modelplaygroundurlid[1:-1]
                 except:
                     import json
                     self.playground_url=json.loads(modelplaygroundurlid)
                     pass
-            
-            if __name__ == '__main__':
-                t1 = Thread(target=deployment_output_information)
-                t2 = Thread(target=run_deployment_code)
 
-                t1.start()
-                t2.start()
 
-                t2.join()  # interpreter will wait until your process get completed or terminated
+            t2 = Thread(target=run_deployment_code(model_filepath=model_filepath, 
+                                          model_type = self.model_type, 
+                                          private = self.private, 
+                                          categorical = self.categorical,
+                                          y_train = y_train, 
+                                          preprocessor_filepath = preprocessor_filepath, 
+                                          example_data = example_data,
+                                          custom_libraries = custom_libraries,
+                                          image=image,
+                                          reproducibility_env_filepath = reproducibility_env_filepath,
+                                          memory=memory,
+                                          timeout=timeout,
+                                          email_list=self.email_list,
+                                          pyspark_support=pyspark_support,
+                                          input_dict=input_dict, 
+                                          print_output=False))
+
+            t2.start()
+
+            t2.join()  # interpreter will wait until your process get completed or terminated
                     #clear last output
-                import os
-                import sys
+            import os
+            import sys
 
-                def cls():
-                    os.system('cls' if os.name=='nt' else 'clear')
+            def cls():
+                os.system('cls' if os.name=='nt' else 'clear')
 
-                # now, to clear the screen
-                cls()
-                from IPython.display import clear_output
-                clear_output()
-                sys.stdout.write('\r')
-                sys.stdout.write("[=====================================] Progress: 100% - Complete!                                            ")
-                sys.stdout.flush()
-                thread_running = False
+            # now, to clear the screen
+            cls()
+            from IPython.display import clear_output
+            clear_output()
+            sys.stdout.write('\r')
+            sys.stdout.write("[=====================================] Progress: 100% - Complete!                                            ")
+            sys.stdout.flush()
+            import json
+            print("\n"+json.loads(successful_deployment_info340893124738241023['body'])[-8]+"\n")
+            print("View live playground now at:\n"+json.loads(successful_deployment_info340893124738241023['body'])[-1])
+
+            print("\nConnect to your playground in Python:\n")
+            print("myplayground=ModelPlayground(playground_url="+json.loads(successful_deployment_info340893124738241023['body'])[-7].replace("Playground Url: ","").strip()+")")
+
+            thread_running = False
+
         else:    
         
             #aws pathway begins here
