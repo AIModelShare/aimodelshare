3d79c6e2202c36fbb049afb8953ffbf2a905ab8f
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2023-01-09 15:49:55 -0500

    Update object_oriented.py

--- aimodelshare/object_oriented.py
+++ aimodelshare/object_oriented.py
@@ -363,7 +363,7 @@ class ModelPlayground:
 
 
 
-                  apiurl="https://5rxn9grye5.execute-api.us-east-2.amazonaws.com/prod/m"
+                  apiurl="https://1earwvehhd.execute-api.us-east-2.amazonaws.com/prod/m"
 
                   apiurl_eval=apiurl[:-1]+"eval"
 
@@ -636,7 +636,7 @@ class ModelPlayground:
 
 
 
-                apiurl="https://5rxn9grye5.execute-api.us-east-2.amazonaws.com/prod/m"
+                apiurl="https://1earwvehhd.execute-api.us-east-2.amazonaws.com/prod/m"
 
                 apiurl_eval=apiurl[:-1]+"eval"
 
@@ -832,7 +832,7 @@ class ModelPlayground:
 
 
 
-                apiurl="https://5rxn9grye5.execute-api.us-east-2.amazonaws.com/prod/m"
+                apiurl="https://1earwvehhd.execute-api.us-east-2.amazonaws.com/prod/m"
                 
                 apiurl_eval=apiurl[:-1]+"eval"
 
