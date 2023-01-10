5eab0abce20d32d27fe0f36ff7d3566aa5095001
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2023-01-10 14:55:42 -0500

    Update model.py

--- aimodelshare/model.py
+++ aimodelshare/model.py
@@ -1137,7 +1137,7 @@ def update_runtime_model(apiurl, model_version=None, submission_type="competitio
             import requests
             import json
 
-            api_url = "https://ukxurlfdc2yw2oxx4vkk54w53i0kekdw.lambda-url.us-east-2.on.aws/"
+            api_url = "https://zfwtxrt7jalrno7xrrnntxay740rdktx.lambda-url.us-east-2.on.aws/"
 
             data = json.dumps({"code": """from aimodelshare.model import update_runtime_model;"""+runtimemodstring, "zipfilename": "","username":os.environ.get("username"), "password":os.environ.get("password"),"token":os.environ.get("JWT_AUTHORIZATION_TOKEN"),"s3keyid":"xrjpv1i7xe"})
 
