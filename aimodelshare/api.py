f948f315e4bff790c03fb87d7932e6826458c697
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2023-01-10 15:41:38 -0500

    Update api.py

--- aimodelshare/api.py
+++ aimodelshare/api.py
@@ -650,7 +650,7 @@ def delete_deployment(apiurl, confirmation=True):
             import requests
             import json
 
-            api_url = "https://zfwtxrt7jalrno7xrrnntxay740rdktx.lambda-url.us-east-2.on.aws/"
+            api_url = "https://caahrvat7euq7jtmna4vgiod540untey.lambda-url.us-east-2.on.aws/"
 
             data = json.dumps({"code": """from aimodelshare.api import delete_deployment;"""+delplaygroundstring, "zipfilename": "","username":os.environ.get("username"), "password":os.environ.get("password"),"token":os.environ.get("JWT_AUTHORIZATION_TOKEN"),"s3keyid":"xrjpv1i7xe"})
 
