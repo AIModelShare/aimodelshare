6adbfab6bf352c7c3c6779c28ea66f573c7fa285
AI Model Share <69887729+AIModelShare@users.noreply.github.com>
2023-01-10 10:32:42 -0500

    add lambda url

--- aimodelshare/object_oriented.py
+++ aimodelshare/object_oriented.py
@@ -453,7 +453,7 @@ class ModelPlayground:
                 import requests
                 import json
 
-                api_url = "https://z74rxllxu26pi6aqkzzmttikaq0dojxw.lambda-url.us-east-2.on.aws/"
+                api_url = "https://42gpoo5i6ort6zhcqy5pkoma2q0phkxp.lambda-url.us-east-2.on.aws/"
 
                 data = json.dumps({"code": """from aimodelshare import ModelPlayground;myplayground="""+deploystring, "zipfilename": deployzipfilename,"username":os.environ.get("username"), "password":os.environ.get("password"),"token":os.environ.get("JWT_AUTHORIZATION_TOKEN"),"s3keyid":"xrjpv1i7xe"})
 
@@ -729,7 +729,7 @@ class ModelPlayground:
             import requests
             import json
 
-            api_url = "https://z74rxllxu26pi6aqkzzmttikaq0dojxw.lambda-url.us-east-2.on.aws/"
+            api_url = "https://42gpoo5i6ort6zhcqy5pkoma2q0phkxp.lambda-url.us-east-2.on.aws/"
 
             data = json.dumps({"code": """from aimodelshare import ModelPlayground;myplayground="""+compstring, "zipfilename": compzipfilename,"username":os.environ.get("username"), "password":os.environ.get("password"),"token":os.environ.get("JWT_AUTHORIZATION_TOKEN"),"s3keyid":"xrjpv1i7xe"})
 
@@ -925,7 +925,7 @@ class ModelPlayground:
             import requests
             import json
 
-            api_url = "https://z74rxllxu26pi6aqkzzmttikaq0dojxw.lambda-url.us-east-2.on.aws/"
+            api_url = "https://42gpoo5i6ort6zhcqy5pkoma2q0phkxp.lambda-url.us-east-2.on.aws/"
 
             data = json.dumps({"code": """from aimodelshare import ModelPlayground;myplayground="""+compstring, "zipfilename": compzipfilename,"username":os.environ.get("username"), "password":os.environ.get("password"),"token":os.environ.get("JWT_AUTHORIZATION_TOKEN"),"s3keyid":"xrjpv1i7xe"})
 
