d102308d99c8a88ea4ad264c5bda07cec669e62a
Heinrich Peters <heinrichpeterz@gmail.com>
2023-03-01 18:01:21 -0500

    data_sharing

--- aimodelshare/__init__.py
+++ aimodelshare/__init__.py
@@ -1,5 +1,6 @@
-from .object_oriented import ModelPlayground, Competition, Experiment, Data
+from .object_oriented import ModelPlayground, Competition,Experiment, Data
 from .preprocessormodules import export_preprocessor,upload_preprocessor,import_preprocessor
+from .data_sharing.download_data import download_data, import_quickstart_data
 from .reproducibility import export_reproducibility_env, import_reproducibility_env
 
 
@@ -12,5 +13,6 @@ __all__ = [
     # Preprocessor
     upload_preprocessor,
     import_preprocessor,
-    export_preprocessor
-    ]
+    export_preprocessor,
+    download_data
+]
