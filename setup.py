31116b569a04ecd2b905d4c9d77b3e8bea5351fc
Heinrich Peters <heinrichpeterz@gmail.com>
2023-03-31 16:30:36 -0400

    added dill

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     install_requires=["boto3==1.26.69", "botocore==1.29.82","scikit-learn==1.2.1","onnx==1.12.0","onnxconverter-common>=1.7.0",
     "regex", "keras2onnx>=1.7.0","tensorflow==2.9.2","tf2onnx","skl2onnx>=1.14.0","onnxruntime>=1.7.0","torch>=1.8.1","pydot==1.3.0",
     "importlib-resources==5.10.0","onnxmltools>=1.6.1","Pympler==0.9","docker==5.0.0","wget==3.2","PyJWT>=2.4.0","seaborn>=0.11.2",
-    "astunparse==1.6.3","shortuuid>=1.0.8","psutil>=5.9.1","pathlib>=1.0.1","scipy==1.7.0", "protobuf==3.19.6"]
+    "astunparse==1.6.3","shortuuid>=1.0.8","psutil>=5.9.1","pathlib>=1.0.1","scipy==1.7.0", "protobuf==3.19.6", "dill"]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
