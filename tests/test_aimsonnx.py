2a012c5e7730487b68bd18bd5c811c8848ee651c
Heinrich Peters <heinrichpeterz@gmail.com>
2023-05-05 15:33:46 -0400

    more tests

--- tests/test_aimsonnx.py
+++ tests/test_aimsonnx.py
@@ -5,6 +5,69 @@ from aimodelshare.aimsonnx import model_from_string
 from aimodelshare.aimsonnx import _get_pyspark_modules
 from aimodelshare.aimsonnx import pyspark_model_from_string
 from aimodelshare.aimsonnx import layer_mapping
+from aimodelshare.aimsonnx import _sklearn_to_onnx
+from aimodelshare.aimsonnx import _pyspark_to_onnx
+from aimodelshare.aimsonnx import _keras_to_onnx
+from aimodelshare.aimsonnx import _pytorch_to_onnx
+from aimodelshare.aimsonnx import _misc_to_onnx
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+import onnx
+from xgboost import XGBClassifier
+from pyspark.ml.classification import RandomForestClassifier, MultilayerPerceptronClassifier
+from keras.models import Sequential
+from torch import nn
+
+def test_sklearn_to_onnx():
+
+
+    model = LogisticRegression(C=10, penalty='l1', solver='liblinear')
+    onnx_model = _sklearn_to_onnx(model)
+    assert isinstance(onnx_model, onnx.ModelProto)
+
+    model = MLPClassifier()
+    onnx_model = _sklearn_to_onnx(model)
+    assert isinstance(onnx_model, onnx.ModelProto)
+
+
+def test_misc_to_onnx():
+
+    model = XGBClassifier()
+    onnx_model = _misc_to_onnx(model)
+    assert isinstance(onnx_model, onnx.ModelProto)
+
+
+def test_pyspark_to_onnx():
+
+    model = RandomForestClassifier()
+    onnx_model = _pyspark_to_onnx(model)
+    assert isinstance(onnx_model, onnx.ModelProto)
+
+    model = MultilayerPerceptronClassifier()
+    onnx_model = _pyspark_to_onnx(model)
+    assert isinstance(onnx_model, onnx.ModelProto)
+
+def test_keras_to_onnx():
+
+    model = Sequential()
+    model.add(Dense(12, input_shape=(8,), activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    onnx_model = _keras_to_onnx(model)
+    assert isinstance(onnx_model, onnx.ModelProto)
+
+
+def test_pytorch_to_onnx():
+
+    model = nn.Sequential(nn.Linear(n_input, n_hidden),
+                          nn.ReLU(),
+                          nn.Linear(n_hidden, n_out),
+                          nn.Sigmoid())
+
+    onnx_model = _pytorch_to_onnx(model)
+    assert isinstance(onnx_model, onnx.ModelProto)
 
 
 def test_get_layer_names():
