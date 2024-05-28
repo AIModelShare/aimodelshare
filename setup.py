9469431de45cfee8b2e3449516ef70f190bde151
Heinrich Peters <heinrichpeterz@gmail.com>
2024-05-27 21:24:54 -0400

    lambda python version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     include_package_data=True,
     #package_data={'': ['placeholders/model.onnx', 'placeholders/preprocessor.zip']},
     )     
